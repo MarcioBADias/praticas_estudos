d3805a5f40375e1646730c02020ad959a1d73238
MarcioBADias <marcio.b.a.dias@gmail.com>
2025-06-22 10:32:13 -0300

    ✅ Criando funções para interar listas e simar ou multiplicar seus valores.

--- bin/praticas_estudos.dart
+++ bin/praticas_estudos.dart
@@ -1,39 +1,31 @@
 import "dart:math";
 
-String generateString(int length) {
-  var random = Random();
-  var stringToGenerate = "";
-  List alphaList = ["a", "b", "c", "d", "e"];
-  for (int i = 0; i < length; i++) {
-    int randomNumber = random.nextInt(20);
-    int alphaListIndex = randomNumber % alphaList.length;
-    var insideAlphaList = alphaList[alphaListIndex];
-    stringToGenerate += insideAlphaList;
+int sumAllElements(List listElements) {
+  var result = 0;
+  for (int i = 0; i < listElements.length; i++) {
+    var insideOfTheList = listElements[i];
+    if (insideOfTheList is int) {
+      result += insideOfTheList;
+    }
   }
-
-  return stringToGenerate;
+  return result;
 }
 
-String generateFullNameAndAge(String fistName, String secondName, int age) {
-  var fullNameAndAge = "Me chamo $fistName $secondName e tenho $age anos";
-  return fullNameAndAge;
+int multiplyAllElements(List listElements) {
+  var result = 1;
+  for (int i = 0; i < listElements.length; i++) {
+    var insideOfTheList = listElements[i];
+    if (insideOfTheList is int) {
+      result *= insideOfTheList;
+    }
+  }
+  return result;
 }
 
 void main() {
-  List mixedList = [];
-
-  for (int i = 0; i < 10; i++) {
-    mixedList.add("string");
-    mixedList.add(i);
-  }
-
-  var randomString01 = generateString(8);
-  var randomString02 = generateString(8);
-  var randomString03 = generateString(8);
-  var generateFullName = generateFullNameAndAge("Marcio", "Dias", 36);
-
-  print(generateFullName);
-  print(
-    "As tres novas senhas são $randomString01 (Senha 01), $randomString02 (Senha02) e $randomString03 (Senha 03)",
-  );
+  List mixedList = [1, 2, 3, 4, 5];
+  int sumOfListElements = sumAllElements(mixedList);
+  int multiplyOfListElements = multiplyAllElements(mixedList);
+  print(sumOfListElements);
+  print(multiplyOfListElements);
 }
